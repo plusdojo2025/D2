5370ff2b3eebf13b258a868e54f5a7219a573dd7
plusdojo2025-yamadamahiro <yamada-mahiro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 14:06:43 +0900

    Update UserDAO.java

--- source/src/main/java/dao/UserDAO.java
+++ source/src/main/java/dao/UserDAO.java
@@ -7,7 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import dto.User;
-
+//ここからserectの文!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
 public class UserDAO {
 	// 引数で指定されたidpwでログイン成功ならtrueを返す
 	public boolean isLoginOK(User idpw) {
@@ -58,5 +58,43 @@ public class UserDAO {
 		// 結果を返す
 		return loginResult;
 	}
+ //ここからinsertの文!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	public boolean insert(User user) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/D2?"
+					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"root", "password");
+
+			// INSERT文を準備する
+			String sql = "INSERT INTO user (id, pw) VALUES (?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, user.getId());
+			pStmt.setString(2, user.getPw());
+
+			// INSERT文を実行
+			int rowsInserted = pStmt.executeUpdate();
+			if (rowsInserted > 0) {
+				result = true;
+			}
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+			result = false;
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return result;
+	}
+
 }
 
