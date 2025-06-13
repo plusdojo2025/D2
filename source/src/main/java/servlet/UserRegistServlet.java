44dd4056db158e81ad7eeb0504f9b87db6b7d858
plusdojo2025-yamadamahiro <yamada-mahiro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 10:52:33 +0900

    Update UserRegistServlet.java

--- source/src/main/java/servlet/UserRegistServlet.java
+++ source/src/main/java/servlet/UserRegistServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDAO;
+import dto.LoginUser;
+import dto.User;
 /**
  * Servlet implementation class UserRegistServlet
  */
@@ -37,9 +41,34 @@ public class UserRegistServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		
+		
+		    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		        throws ServletException, IOException {
+
+		        request.setCharacterEncoding("UTF-8");
+		    	String id = request.getParameter("id");
+				String pw = request.getParameter("pw");
+
+		        User user = new User(id, pw);
+		        UserDAO dao = new UserDAO();
+		        boolean success = dao.insert(user);
+
+		        response.setContentType("text/html; charset=UTF-8");
+		        PrintWriter out = response.getWriter();
+		        out.println("<html><body>");
+
+		        if (success) {
+		            out.println("<h2>登録成功！</h2>");
+		            out.println("<a href=\"login.jsp\">ログインページへ</a>");
+		        } else {
+		            out.println("<h2 style='color:red;'>登録に失敗しました。</h2>");
+		        }
+
+		        out.println("</body></html>");
+		    }
+		
 	}
 
 }
