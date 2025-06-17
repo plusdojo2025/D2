cd1209eae5b93603f3caf66b91e5b15d27569281
plusdojo2025-yamadamahiro <yamada-mahiro-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-17 09:32:03 +0900

    Update HistoryServlet.java

--- source/src/main/java/servlet/HistoryServlet.java
+++ source/src/main/java/servlet/HistoryServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.HistoryDAO;
+import dto.History;
 
 
 /**
@@ -38,11 +41,14 @@ public class HistoryServlet extends HttpServlet {
 		
 		
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String keyword = req.getParameter("searchDate");
+        HistoryDAO dao = new HistoryDAO();
+        List<History> files = dao.getFilesByDate(keyword);
+
+        req.setAttribute("fileList", files);
+        req.getRequestDispatcher("/History.jsp").forward(req, resp);
 		//ヘルプはjavascriptで表示!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		
 		
 		
 		
