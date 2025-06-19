bc468c9a7ef9e974083081656ea092930562de57
plusdojo2025-kimura <kimura-kyousuke-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-19 16:29:13 +0900

    deleteの追加

--- source/src/main/java/dao/HealthRecordDAO.java
+++ source/src/main/java/dao/HealthRecordDAO.java
@@ -228,8 +228,8 @@ public class HealthRecordDAO {
 		return RecordList;
 	}
 
-	/*// deleteメソッド：引数cardで指定されたレコードを削除し、成功したらtrueを返す
-	public boolean delete(String userId, int year, int month ) {
+	// deleteメソッド：引数cardで指定されたレコードを削除し、成功したらtrueを返す
+	public boolean delete(String userId, int year, int month) {
 		Connection conn = null;
 		boolean result = false;
 		try {
@@ -240,224 +240,198 @@ public class HealthRecordDAO {
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/d2?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 					"root", "password");
-			
-			// SQL文を準備する 2025-06-12
-			String sql = "DELETE * FROM health_whole where user_id = ?, date like ?";
-			//pStmtWhole.setInt(2, year+ "-" + month + "-%" );*/
-			
-	/*// updateメソッド：引数cardで指定されたレコードを更新し、成功したらtrueを返す
-	public boolean update(HealthRecord record) {
-		Connection conn = null;
-		boolean result = false;
 
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/d2?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
-			
 			// トランザクション開始
-	        conn.setAutoCommit(false);
-
-			
-			try{
-				// 1つ目の健康記録（全体）に記録するSQL文を準備する
-				String sqlWhole = "UPDATE SET UPDATE health_whole SET nosmoke=? sleep_time=? calorie_intake=? WHERE user_id=?, date=? ";
-				PreparedStatement pStmtWhole = conn.prepareStatement(sqlWhole);
-				// SQL文を完成させる
-				// 禁煙できたかを上書きする
-				pStmtWhole.setInt(1, record.getNosmoke());
-
-				// 睡眠時間を上書きする
-				pStmtWhole.setDouble(2, record.getSleepHours());
-
-				// 摂取カロリーを上書きする
-				pStmtWhole.setInt(3, record.getCalorieIntake());
-
-				// ユーザーIDを指定する
-				pStmtWhole.setString(4, record.getUserId());
-
-				// 日付を指定する
-				pStmtWhole.setString(5, record.getDate());
+			conn.setAutoCommit(false);
+			String date ;
+			if (month >= 1 && month <= 9) {
+				date = year + "-0" + month + "-%";
+			} else {
+				date = year + "-" + month + "-%";
+			}
+			// ２つ目の健康記録（アルコール）を削除するSQL文を準備して完成させる
+			String sqlAlcohol = "DELETE FROM health_alcohol WHERE user_id = ? AND date like ?";
+			try (PreparedStatement pStmtAlcohol = conn.prepareStatement(sqlAlcohol)) {
+				pStmtAlcohol.setString(1, userId);
 				
-				//SQLを実行する
+				pStmtAlcohol.setString(2, date);
+				// SQL文を実行
+				pStmtAlcohol.executeUpdate();
+			}
+			// 3つ目の健康記録（運動）を削除するSQL文を準備して完成させる
+			String sqlExercise = "DELETE FROM health_exercise where user_id = ? AND date like ?";
+			try (PreparedStatement pStmtExercise = conn.prepareStatement(sqlExercise)) {
+				pStmtExercise.setString(1, userId);
+				pStmtExercise.setString(2, date);
+				// SQL文を実行
+				pStmtExercise.executeUpdate();
+			}
+			// １つ目の健康記録（全体）を削除するSQL文を準備して完成させる
+			String sqlWhole = "DELETE FROM health_whole WHERE user_id = ? AND date like ?";
+			try (PreparedStatement pStmtWhole = conn.prepareStatement(sqlWhole)) {
+				pStmtWhole.setString(1, userId);
+				pStmtWhole.setString(2, date);
+				// SQL文を実行
 				pStmtWhole.executeUpdate();
 			}
-			
-			try {
-				// 2つ目の健康記録(アルコール)に記録するSQL文を準備する
-				// まず健康記録（アルコール）で指定されたユーザー、日付のレコードを削除する
-				String sqlDelAlc = "DELETE FROM health_alcohol WHERE user_id=?, date=? ";
-				PreparedStatement pStmtDelAlc = conn.prepareStatement(sqlDelAlc);
-
-				// ユーザーIDを指定する
-				pStmtDelAlc.setString(1, record.getUserId());
-
-				// 日付を指定する
-				pStmtDelAlc.setString(2, record.getDate());
-			}
-			
-			try {
-				// 健康記録（アルコール）に新規レコードを追加
-				String sqlInsAlc = "INSERT INTO health_alcohol(user_id, date, pure_alcohol_consumed, alcohol_consumed, alcohol_content)"
-						+ "VALUES(?, ?, ?, ?, ?)";
-				PreparedStatement pStmtInsAlc = conn.prepareStatement(sqlInsAlc);
-
-				// SQL文を完成させる
-				// ユーザーIDを？に挿入
-				if (record.getUserId() != null) {
-					pStmtInsAlc.setString(1, record.getUserId());
-				} else {
-					pStmtInsAlc.setString(1, "");
-				}
-				// 日付を？に挿入
-				if (record.getDate() != null) {
-					pStmtInsAlc.setString(2, record.getDate());
-				} else {
-					pStmtInsAlc.setString(2, "");
-				}
-				// 純アルコール摂取量を？に挿入
-				pStmtInsAlc.setDouble(3, record.getPureAlcoholConsumed());
-
-				// 酒摂取量を？に挿入
-				pStmtInsAlc.setInt(4, record.getAlcoholConsumed());
-
-				// アルコール度数を？に挿入
-				pStmtInsAlc.setDouble(5, record.getAlcoholContent());
-			}
-			
-			try {
-				// 3つ目の健康記録(運動)に記録するSQL文を準備する
-				// まず健康記録（運動）で指定されたユーザー、日付のレコードを削除する
-				String sqlDelExer = "DELETE FROM health_alcohol WHERE user_id=?, date=? ";
-				PreparedStatement pStmtDelExer = conn.prepareStatement(sqlDelExer);
-
-				// ユーザーIDを指定する
-				pStmtDelExer.setString(1, record.getUserId());
-
-				// 日付を指定する
-				pStmtDelExer.setString(2, record.getDate());
-			}
-			
-			
-		}
-			
-		
-	    
-
-			// 健康記録（アルコール）に新規レコードを追加
-			String sql3 = "INSERT INTO health_alcohol(user_id, date, pure_alcohol_consumed, alcohol_consumed, alcohol_content)"
-					+ "VALUES(?, ?, ?, ?, ?)";
-			PreparedStatement pStmt3 = conn.prepareStatement(sql3);
-
-			// SQL文を完成させる
-			// ユーザーIDを？に挿入
-			if (record.getUserId() != null) {
-				pStmt3.setString(1, record.getUserId());
-			} else {
-				pStmt3.setString(1, "");
-			}
-			// 日付を？に挿入
-			if (record.getDate() != null) {
-				pStmt3.setString(2, record.getDate());
-			} else {
-				pStmt3.setString(2, "");
-			}
-			// 純アルコール摂取量を？に挿入
-			pStmt3.setDouble(3, record.getPureAlcoholConsumed());
-
-			// 酒摂取量を？に挿入
-			pStmt3.setInt(4, record.getAlcoholConsumed());
-
-			// アルコール度数を？に挿入
-			pStmt3.setDouble(5, record.getAlcoholContent());
-			
-		}catch(
-
-	SQLException alcoholEx)
-	{
-		System.out.println("アルコール処理でエラー：ロールバックします");
-		conn.rollback(sp1); // アルコール処理のみをロールバック
-	}
-
-	// セーブポイント②（ここから運動の処理）
-	Savepoint sp2 = conn.setSavepoint("ExerciseStart");
-
-	try
-	{
-
-		// 3つ目の健康記録(運動)に記録するSQL文を準備する
-		// まず健康記録（運動）で指定されたユーザー、日付のレコードを削除する
-		String sql4 = "DELETE FROM health_exercise WHERE user_id=?, date=? ";
-		PreparedStatement pStmt4 = conn.prepareStatement(sql4);
-
-		// ユーザーIDを指定する
-		pStmt4.setString(4, record.getUserId());
-
-		// 日付を指定する
-		pStmt4.setString(5, record.getDate());
-
-		// 健康記録（アルコール）に新規レコードを追加
-		String sql5 = "INSERT INTO health_exercise(user_id, date, calorie_consu, exercise_type, exercise_time)"
-				+ "VALUES(?, ?, ?, ?, ?)";
-		PreparedStatement pStmt5 = conn.prepareStatement(sql5);
-
-		// SQL文を完成させる
-		// ユーザーIDを？に挿入
-		if (record.getUserId() != null) {
-			pStmt5.setString(1, record.getUserId());
-		} else {
-			pStmt5.setString(1, "");
-		}
-		// 日付を？に挿入
-		if (record.getDate() != null) {
-			pStmt5.setString(2, record.getDate());
-		} else {
-			pStmt5.setString(2, "");
-		}
-
-		// 消費カロリーを？に挿入
-		pStmt5.setDouble(3, record.getCalorieConsu());
-
-		// 運動の種類を？に挿入
-		if (record.getExerciseType() != null) {
-			pStmt5.setString(4, record.getExerciseType());
-		} else {
-			pStmt5.setString(4, "");
-		}
-
-		// 運動時間を？に挿入
-		pStmt5.setInt(5, record.getExerciseTime());
-
-		// SQL文を実行する(運動記録は１件だけとは限らないからこれじゃだめじゃね？)
-		if (pStmt1.executeUpdate() == 1 && pStmt2.executeUpdate() == 1 && pStmt3.executeUpdate() == 1) {
+			// 全て成功したらコミット
+			conn.commit();
 			result = true;
-		}
-
-	}catch(
-	SQLException e)
-	{
-		e.printStackTrace();
-	}catch(
-	ClassNotFoundException e)
-	{
-		e.printStackTrace();
-	}finally
-	{
-		// データベースを切断
-		if (conn != null) {
-			try {
-				conn.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+			if (conn != null) {
+				try {
+					conn.rollback();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.setAutoCommit(true); // 元に戻す
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
 		}
+
+		// 結果を返す
+		return result;
 	}
 
-	// 結果を返す
-	return result;
-  */
+	public static void main(String[] args) {
+		HealthRecordDAO DAO = new HealthRecordDAO();
+		DAO.delete("kazutoshi_t", 2025, 06);
+
+	}
+	/*
+	 * // updateメソッド：引数cardで指定されたレコードを更新し、成功したらtrueを返す public boolean
+	 * update(HealthRecord record) { Connection conn = null; boolean result = false;
+	 * 
+	 * try { // JDBCドライバを読み込む Class.forName("com.mysql.cj.jdbc.Driver");
+	 * 
+	 * // データベースに接続する conn =
+	 * DriverManager.getConnection("jdbc:mysql://localhost:3306/d2?" +
+	 * "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+	 * "root", "password");
+	 * 
+	 * // トランザクション開始 conn.setAutoCommit(false);
+	 * 
+	 * 
+	 * try{ // 1つ目の健康記録（全体）に記録するSQL文を準備する String sqlWhole =
+	 * "UPDATE SET UPDATE health_whole SET nosmoke=? sleep_time=? calorie_intake=? WHERE user_id=?, date=? "
+	 * ; PreparedStatement pStmtWhole = conn.prepareStatement(sqlWhole); //
+	 * SQL文を完成させる // 禁煙できたかを上書きする pStmtWhole.setInt(1, record.getNosmoke());
+	 * 
+	 * // 睡眠時間を上書きする pStmtWhole.setDouble(2, record.getSleepHours());
+	 * 
+	 * // 摂取カロリーを上書きする pStmtWhole.setInt(3, record.getCalorieIntake());
+	 * 
+	 * // ユーザーIDを指定する pStmtWhole.setString(4, record.getUserId());
+	 * 
+	 * // 日付を指定する pStmtWhole.setString(5, record.getDate());
+	 * 
+	 * //SQLを実行する pStmtWhole.executeUpdate(); }
+	 * 
+	 * try { // 2つ目の健康記録(アルコール)に記録するSQL文を準備する //
+	 * まず健康記録（アルコール）で指定されたユーザー、日付のレコードを削除する String sqlDelAlc =
+	 * "DELETE FROM health_alcohol WHERE user_id=?, date=? "; PreparedStatement
+	 * pStmtDelAlc = conn.prepareStatement(sqlDelAlc);
+	 * 
+	 * // ユーザーIDを指定する pStmtDelAlc.setString(1, record.getUserId());
+	 * 
+	 * // 日付を指定する pStmtDelAlc.setString(2, record.getDate()); }
+	 * 
+	 * try { // 健康記録（アルコール）に新規レコードを追加 String sqlInsAlc =
+	 * "INSERT INTO health_alcohol(user_id, date, pure_alcohol_consumed, alcohol_consumed, alcohol_content)"
+	 * + "VALUES(?, ?, ?, ?, ?)"; PreparedStatement pStmtInsAlc =
+	 * conn.prepareStatement(sqlInsAlc);
+	 * 
+	 * // SQL文を完成させる // ユーザーIDを？に挿入 if (record.getUserId() != null) {
+	 * pStmtInsAlc.setString(1, record.getUserId()); } else {
+	 * pStmtInsAlc.setString(1, ""); } // 日付を？に挿入 if (record.getDate() != null) {
+	 * pStmtInsAlc.setString(2, record.getDate()); } else { pStmtInsAlc.setString(2,
+	 * ""); } // 純アルコール摂取量を？に挿入 pStmtInsAlc.setDouble(3,
+	 * record.getPureAlcoholConsumed());
+	 * 
+	 * // 酒摂取量を？に挿入 pStmtInsAlc.setInt(4, record.getAlcoholConsumed());
+	 * 
+	 * // アルコール度数を？に挿入 pStmtInsAlc.setDouble(5, record.getAlcoholContent()); }
+	 * 
+	 * try { // 3つ目の健康記録(運動)に記録するSQL文を準備する // まず健康記録（運動）で指定されたユーザー、日付のレコードを削除する
+	 * String sqlDelExer = "DELETE FROM health_alcohol WHERE user_id=?, date=? ";
+	 * PreparedStatement pStmtDelExer = conn.prepareStatement(sqlDelExer);
+	 * 
+	 * // ユーザーIDを指定する pStmtDelExer.setString(1, record.getUserId());
+	 * 
+	 * // 日付を指定する pStmtDelExer.setString(2, record.getDate()); }
+	 * 
+	 * 
+	 * }
+	 * 
+	 * 
+	 * 
+	 * 
+	 * // 健康記録（アルコール）に新規レコードを追加 String sql3 =
+	 * "INSERT INTO health_alcohol(user_id, date, pure_alcohol_consumed, alcohol_consumed, alcohol_content)"
+	 * + "VALUES(?, ?, ?, ?, ?)"; PreparedStatement pStmt3 =
+	 * conn.prepareStatement(sql3);
+	 * 
+	 * // SQL文を完成させる // ユーザーIDを？に挿入 if (record.getUserId() != null) {
+	 * pStmt3.setString(1, record.getUserId()); } else { pStmt3.setString(1, ""); }
+	 * // 日付を？に挿入 if (record.getDate() != null) { pStmt3.setString(2,
+	 * record.getDate()); } else { pStmt3.setString(2, ""); } // 純アルコール摂取量を？に挿入
+	 * pStmt3.setDouble(3, record.getPureAlcoholConsumed());
+	 * 
+	 * // 酒摂取量を？に挿入 pStmt3.setInt(4, record.getAlcoholConsumed());
+	 * 
+	 * // アルコール度数を？に挿入 pStmt3.setDouble(5, record.getAlcoholContent());
+	 * 
+	 * }catch(
+	 * 
+	 * SQLException alcoholEx) { System.out.println("アルコール処理でエラー：ロールバックします");
+	 * conn.rollback(sp1); // アルコール処理のみをロールバック }
+	 * 
+	 * // セーブポイント②（ここから運動の処理） Savepoint sp2 = conn.setSavepoint("ExerciseStart");
+	 * 
+	 * try {
+	 * 
+	 * // 3つ目の健康記録(運動)に記録するSQL文を準備する // まず健康記録（運動）で指定されたユーザー、日付のレコードを削除する String
+	 * sql4 = "DELETE FROM health_exercise WHERE user_id=?, date=? ";
+	 * PreparedStatement pStmt4 = conn.prepareStatement(sql4);
+	 * 
+	 * // ユーザーIDを指定する pStmt4.setString(4, record.getUserId());
+	 * 
+	 * // 日付を指定する pStmt4.setString(5, record.getDate());
+	 * 
+	 * // 健康記録（アルコール）に新規レコードを追加 String sql5 =
+	 * "INSERT INTO health_exercise(user_id, date, calorie_consu, exercise_type, exercise_time)"
+	 * + "VALUES(?, ?, ?, ?, ?)"; PreparedStatement pStmt5 =
+	 * conn.prepareStatement(sql5);
+	 * 
+	 * // SQL文を完成させる // ユーザーIDを？に挿入 if (record.getUserId() != null) {
+	 * pStmt5.setString(1, record.getUserId()); } else { pStmt5.setString(1, ""); }
+	 * // 日付を？に挿入 if (record.getDate() != null) { pStmt5.setString(2,
+	 * record.getDate()); } else { pStmt5.setString(2, ""); }
+	 * 
+	 * // 消費カロリーを？に挿入 pStmt5.setDouble(3, record.getCalorieConsu());
+	 * 
+	 * // 運動の種類を？に挿入 if (record.getExerciseType() != null) { pStmt5.setString(4,
+	 * record.getExerciseType()); } else { pStmt5.setString(4, ""); }
+	 * 
+	 * // 運動時間を？に挿入 pStmt5.setInt(5, record.getExerciseTime());
+	 * 
+	 * // SQL文を実行する(運動記録は１件だけとは限らないからこれじゃだめじゃね？) if (pStmt1.executeUpdate() == 1 &&
+	 * pStmt2.executeUpdate() == 1 && pStmt3.executeUpdate() == 1) { result = true;
+	 * }
+	 * 
+	 * }catch( SQLException e) { e.printStackTrace(); }catch( ClassNotFoundException
+	 * e) { e.printStackTrace(); }finally { // データベースを切断 if (conn != null) { try {
+	 * conn.close(); } catch (SQLException e) { e.printStackTrace(); } } }
+	 * 
+	 * // 結果を返す return result;
+	 */
 }
