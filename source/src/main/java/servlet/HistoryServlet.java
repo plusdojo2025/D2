97ba60e79615fc3c9f014d87cf15a60290a3fedb
plusdojo2025-kimura <kimura-kyousuke-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-23 10:25:13 +0900

    HistoryServletの機能完成！！

--- source/src/main/java/servlet/HistoryServlet.java
+++ source/src/main/java/servlet/HistoryServlet.java
@@ -34,7 +34,7 @@ public class HistoryServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		boolean test = true;
 		String userId = "kazutoshi_t";
 		/*
 		 * 今日の西暦と月から、12か月以上前の健康記録と報酬達成日のレコードをそれぞれのテーブルから削除する HealthRecordDAOのdelete(),
@@ -73,24 +73,28 @@ public class HistoryServlet extends HttpServlet {
 		// 報酬達成日のレコードを削除
 		RewardDayDAO rdDao = new RewardDayDAO();
 		List<String> dateList2 = rdDao.select(userId); // 全ての健康記録の日付だけ古い順に取得
-		Date oldestDayTmp2 = Date.valueOf(dateList2.get(0));// 一番古い日付を取得 2024-04-01
-		LocalDate oldestDay2 = oldestDayTmp2.toLocalDate();
-		// 一番古い報酬達成日の年月を取得
-		int oldestDayYear2 = oldestDay2.getYear();// 2024
-		int oldestDayMonth2 = oldestDay2.getMonthValue();// 4
-		if (todayYear - oldestDayYear2 >= 1) {
-			gap = 12 * (todayYear - oldestDayYear2) + (todayMonth - oldestDayMonth2);
-		}
-		// 削除工程
-		if (gap >= 12) {
-			int loopCount = gap - 12 + 1;
-			for (int i = 0; i < loopCount; i++) {
-				if (oldestDayMonth2 + i > 12) {
-					rdDao.delete(userId, oldestDayYear2 + 1, oldestDayMonth2 + i - 12);
-				} else {
-					rdDao.delete(userId, oldestDayYear2, oldestDayMonth2 + i);
+		try {
+			Date oldestDayTmp2 = Date.valueOf(dateList2.get(0));// 一番古い日付を取得 2024-04-01
+			LocalDate oldestDay2 = oldestDayTmp2.toLocalDate();
+			// 一番古い報酬達成日の年月を取得
+			int oldestDayYear2 = oldestDay2.getYear();// 2024
+			int oldestDayMonth2 = oldestDay2.getMonthValue();// 4
+			if (todayYear - oldestDayYear2 >= 1) {
+				gap = 12 * (todayYear - oldestDayYear2) + (todayMonth - oldestDayMonth2);
+			}
+			// 削除工程
+			if (gap >= 12) {
+				int loopCount = gap - 12 + 1;
+				for (int i = 0; i < loopCount; i++) {
+					if (oldestDayMonth2 + i > 12) {
+						rdDao.delete(userId, oldestDayYear2 + 1, oldestDayMonth2 + i - 12);
+					} else {
+						rdDao.delete(userId, oldestDayYear2, oldestDayMonth2 + i);
+					}
 				}
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		/*
@@ -113,24 +117,47 @@ public class HistoryServlet extends HttpServlet {
 				if (oldestDayMonth3 + i > 12) {// 2024-03 2024-05
 					if (pointList.get(j).getYear() == (oldestDayYear3 + 1)
 							&& pointList.get(j).getMonth() == (oldestDayMonth3 + i)) {
-						try {
-				            // 新しいファイルのインスタンスを作成
-				            File file = new File("history/" + userId + "/" + (oldestDayYear3 + 1) + "-"
-									+ (oldestDayMonth3 + i) + ".txt");
 
-				            // ファイルが存在しない場合、新規作成
-				            if (file.createNewFile()) {
-				                System.out.println("ファイルが作成されました: " + file.getName());
-				            } else {
-				                System.out.println("ファイルは既に存在します。");
-				            }
-				        } catch (IOException e) {
-				            System.out.println("エラーが発生しました。");
-				            e.printStackTrace();
-				        }
-						
-						try (FileWriter writer = new FileWriter("history/" + userId + "/" + (oldestDayYear3 + 1) + "-"
-								+ (oldestDayMonth3 + i) + ".txt")) {
+						String filename;
+						String folderPath;
+						if (test) {
+							folderPath = "c:/plusdojo2025/" + request.getContextPath() + "/source/src/main/webapp/"
+									+ "history/" + userId ; 
+							filename = (oldestDayYear3 + 1) + "-" + (oldestDayMonth3 + i) + ".txt";
+
+						} else {
+							folderPath = request.getContextPath() + "/source/src/main/webapp/" + "history/" + userId ;
+							filename = (oldestDayYear3 + 1) + "-" + (oldestDayMonth3 + i) + ".txt";
+									
+						}
+						String filePath = folderPath + "/" + filename;
+						try {
+
+							File folder, file;
+
+							folder = new File(folderPath);
+							 file  = new File(filePath);
+
+							// フォルダの作成
+							if (folder.mkdirs()) {
+								System.out.println("フォルダが作成されました: " + file.getName());
+							} else {
+								System.out.println("フォルダは既に存在します。");
+							}
+
+							// ファイルの作成
+							if (file.createNewFile()) {
+								System.out.println("ファイルが作成されました: " + file.getName());
+							} else {
+								System.out.println("ファイルは既に存在します。");
+							}
+
+						} catch (IOException e) {
+							System.out.println("エラーが発生しました。");
+							e.printStackTrace();
+						}
+
+						try (FileWriter writer = new FileWriter(filePath)) {
 							writer.write(pointList.get(i).getUser_id() + "," + pointList.get(i).getYear() + ","
 									+ pointList.get(i).getMonth() + "," + pointList.get(i).getTotal_calorie_consumed()
 									+ "," + pointList.get(i).getTotal_nosmoke() + ","
@@ -142,34 +169,56 @@ public class HistoryServlet extends HttpServlet {
 							e.printStackTrace();
 						}
 						HistoryDAO hDao = new HistoryDAO();
-						History history = new History(userId, oldestDayYear + 1, oldestDayMonth + i, "history/" + userId
-								+ "/" + (oldestDayYear3 + 1) + "-" + (oldestDayMonth3 + i) + ".txt");
+						History history = new History(userId, oldestDayYear3 + 1, oldestDayMonth3 + i, filename);
 						hDao.insert(history);
 
-						pDao.delete(userId, oldestDayYear3 + 1, oldestDayMonth + i - 12);
+						pDao.delete(userId, oldestDayYear3 + 1, oldestDayMonth3 + i - 12);
 						j++;
 					}
 
 				} else {
 					if (pointList.get(j).getYear() == oldestDayYear3
 							&& pointList.get(j).getMonth() == (oldestDayMonth3 + i)) {
-						try {
-				            // 新しいファイルのインスタンスを作成
-				            File file = new File("history/" + userId + "/" + oldestDayYear3 + "-"
-									+ (oldestDayMonth3 + i) + ".txt");
+						String filename;
+						String folderPath;
+						if (test) {
+							folderPath = "c:/plusdojo2025/" + request.getContextPath() + "/source/src/main/webapp/"
+									+ "history/" + userId ; 
+							filename = (oldestDayYear3) + "-" + (oldestDayMonth3 + i) + ".txt";
 
-				            // ファイルが存在しない場合、新規作成
-				            if (file.createNewFile()) {
-				                System.out.println("ファイルが作成されました: " + file.getName());
-				            } else {
-				                System.out.println("ファイルは既に存在します。");
-				            }
-				        } catch (IOException e) {
-				            System.out.println("エラーが発生しました。");
-				            e.printStackTrace();
-				        }
+						} else {
+							folderPath = request.getContextPath() + "/source/src/main/webapp/" + "history/" + userId ;
+							filename = (oldestDayYear3) + "-" + (oldestDayMonth3 + i) + ".txt";
+									
+						}
+						String filePath = folderPath + "/" + filename;
+						try {
+
+							File folder, file;
+
+							folder = new File(folderPath);
+							 file  = new File(filePath);
+
+							// フォルダの作成
+							if (folder.mkdirs()) {
+								System.out.println("フォルダが作成されました: " + file.getName());
+							} else {
+								System.out.println("フォルダは既に存在します。");
+							}
+
+							// ファイルの作成
+							if (file.createNewFile()) {
+								System.out.println("ファイルが作成されました: " + file.getName());
+							} else {
+								System.out.println("ファイルは既に存在します。");
+							}
+
+						} catch (IOException e) {
+							System.out.println("エラーが発生しました。");
+							e.printStackTrace();
+						}
 						try (FileWriter writer = new FileWriter(
-								"history/" + userId + "/" + oldestDayYear3 + "-" + (oldestDayMonth3 + i) + ".txt")) {
+								filePath)) {
 							writer.write(pointList.get(i).getUser_id() + "," + pointList.get(i).getYear() + ","
 									+ pointList.get(i).getMonth() + "," + pointList.get(i).getTotal_calorie_consumed()
 									+ "," + pointList.get(i).getTotal_nosmoke() + ","
@@ -181,10 +230,9 @@ public class HistoryServlet extends HttpServlet {
 							e.printStackTrace();
 						}
 						HistoryDAO hDao = new HistoryDAO();
-						History history = new History(userId, oldestDayYear, oldestDayMonth + i, "history/" + userId
-								+ "/" + (oldestDayYear3 + 1) + "-" + (oldestDayMonth3 + i) + ".txt");
+						History history = new History(userId, oldestDayYear3 , oldestDayMonth3 + i, filename);
 						hDao.insert(history);
-						pDao.delete(userId, oldestDayYear3, oldestDayMonth + i);
+						pDao.delete(userId, oldestDayYear3, oldestDayMonth3 + i);
 						j++;
 					}
 				}
@@ -200,6 +248,7 @@ public class HistoryServlet extends HttpServlet {
 		// ログインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/history.jsp");
 		dispatcher.forward(request, response);
+		
 
 	}
 
