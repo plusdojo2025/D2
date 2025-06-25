2b24565af0237c3cbfee8694a66aac3688f3919a
plusdojo2025takahashi <takahashi-taisei-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-25 11:53:48 +0900

    ポップアップの一部機能追加

--- source/src/main/webapp/js/home.js
+++ source/src/main/webapp/js/home.js
@@ -1,8 +1,8 @@
-"use strict"
-
-window.onload = function () {
-    const needsInput = "${needsInput}" === "true";
-    if (needsInput) {
-        document.getElementById("popup").style.display = "block";
-    }
-};
+document.addEventListener('wheel', function(event) {
+	const target = event.target;
+	if (target.tagName.toLowerCase() === 'input' && target.type === 'number') {
+		event.preventDefault();
+		const delta = Math.sign(event.deltaY);
+		target.stepUp(-delta);
+	}
+}, { passive: false });
